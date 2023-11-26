73d5d9efb5cc83326f198eab5964ead3a5703f66
GianBeltran <Gbeltranl0506@gmail.com>
2023-11-25 21:16:54 -0500

    Primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL3_BeltranLopez/DswiiCl3BeltranLopezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL3_BeltranLopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl3BeltranLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl3BeltranLopezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL3_BeltranLopez/DswiiCl3BeltranLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL3_BeltranLopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl3BeltranLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
